888176c05613b26e4f63421098bd44f43ed2e3fb
acid <oreoigniter@gmail.com>
2025-01-26 21:01:25 +0300

    Add a lot of string manipulation and comparison functions

--- string.c
+++ string.c
@@ -3,6 +3,7 @@
 
 #include <string.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 String sv(char* string) {
     return (String) { .bytes = string, .size = strlen(string) };
@@ -32,3 +33,30 @@ String sv_split(String* string, String delim) {
     return result;
 }
 
+void sv_trim_left(String* string) {
+    while (string->size > 0 && isspace(string->bytes[0])) string->bytes++;
+}
+
+void sv_trim_right(String* string) {
+    while (string->size > 0 && isspace(string->bytes[string->size-1])) string->size--;
+}
+
+void sv_trim(String* string) {
+    sv_trim_left(string);
+    sv_trim_right(string);
+}
+
+bool sv_compare(String a, String b) {
+    if (a.size != b.size) return false;
+    return memcpy(a.bytes, b.bytes, a.size) == 0;
+}
+
+bool sv_startswith(String string, String prefix) {
+    if (string.size < prefix.size) return false;
+    return memcpy(string.bytes, prefix.bytes, prefix.size) == 0;
+}
+
+bool sv_endswith(String string, String suffix) {
+    if (string.size < suffix.size) return false;
+    return memcpy(string.bytes + (string.size - suffix.size), suffix.bytes, suffix.size) == 0;
+}
--- string.h
+++ string.h
@@ -3,6 +3,7 @@
 #define STRING_H_
 
 #include <stdint.h>
+#include <stdbool.h>
 
 typedef struct {
     char* bytes;
@@ -12,7 +13,13 @@ typedef struct {
 String sv(char* string);
 String sv_from_bytes(char* bytes, size_t size);
 char* sv_to_cstr(String string); // TO BE FREED !!!!
-String sv_split(String* string, String delim);
+String sv_split(String* string, String delim); 
+void sv_trim_left(String* string);
+void sv_trim_right(String* string);
+void sv_trim(String* string);
+bool sv_compare(String a, String b);
+bool sv_startswith(String string, String prefix);
+bool sv_endswith(String string, String suffix);
 
 #define SV_FMT "%.*s"
 #define SvFmt(string) (int) string.size, string.bytes
